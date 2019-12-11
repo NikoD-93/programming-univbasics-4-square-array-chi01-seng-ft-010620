3decda23451a8dadde757294e86e245d414fbc74
Kieron Niko Dixon <nikodixon@gmail.com>
2019-12-11 16:19:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,11 @@ def square_array(array)
   counter = 0
   new_array = []
   square = array[counter]**2
+  
   while counter < array.length do
   new_array << square
   counter += 1
+end
 end
 
 puts new_array
